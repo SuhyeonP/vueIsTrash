d48d5f2c3f4fd471c3c449226605f1b14f9c06b2
suhyeonP <ahah12k@gmail.com>
2021-07-05 14:05:32 +0900

    feat: add vuex

---
+++ std-vuex/src/views/BasicStore.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>count:  {{count}}</p>
+  <button type="button" @click="increment">increment</button>
+</template>
+
+<script>
+export default {
+  name: "BasicStore",
+  components: {},
+  methods: {
+    increment () {
+      this.$store.commit('increment');
+    }
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
+  data() {
+  },
+  setup() {
+  },
+  created() {
+  },
+  mounted() {
+  },
+  unmounted() {
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
