6dd873503cb802d04b1db51d65811f0d57dc9ea5
suhyeon <ahah12k@gmail.com>
2021-08-29 09:03:39 +0900

    feat: add data loader template

--- temp/src/App.vue
+++ temp/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <TestCode />
+  <v-container>
+    <TestCode />
+    <br />
+    <DataLoader />
+  </v-container>
 </template>
 
 <script>
 import TestCode from './components/TestCode.vue';
-
+import DataLoader from './components/DataLoader.vue';
 export default {
   name: 'App',
 
   components: {
     TestCode,
+    DataLoader,
   },
 
   data: () => ({
